3115c131facd4dd98129f0d637fe2c56516df392
terence.woodhall@seaglasscloud.com <terence.woodhall@seaglasscloud.com>
2019-11-14 21:01:26 +0000

    Setting up our React app

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+
+export default function Routes() {
+  return (
+    <Switch>
+      { /* Finally, catch all unmatched routes */ }
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-1",
+      BUCKET: "Ynotes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "eu-west-1",
+      URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: "eu-west-1_sGUxNK8VA",
+      APP_CLIENT_ID: "1vl1hpj8a9ibpfrf9ka9m9qogj",
+      IDENTITY_POOL_ID: "eu-west-1:1a749f12-dda0-406a-afa9-a36d80e716d4"
+    }
+  };
